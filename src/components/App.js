97d3b4f277867c1c29a99cd167e0aaa2e8e1c7ca
Aquib <aquibshaikh02397@gmail.com>
2020-11-21 19:09:12 +0530

    added navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,94 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import {
+    BrowserRouter ,
+    Switch,
+    Route,
+    Link,
+    useLocation
+  } from "react-router-dom";
 
+  
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
+    constructor(props){
+        super(props);
     }
+    
+    render() {
+        console.log("dde");
+        
+
+        return (
+          <div id="main">
+            <BrowserRouter>
+              <div>
+                <nav>
+                  <ul>
+                    <li>
+                      <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                      <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                      <Link to="/aa">not linked</Link>
+                    </li>
+                    <li>
+                      <Link to="/aae">bkl</Link>
+                    </li>
+                  </ul>
+                </nav>
+                <LocationDisplay />
+                Hello
+              </div>
+              <Switch>
+                <Route path="/about">
+                  <About />
+                </Route>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="*">
+                  <NoMatch />
+                </Route>
+              </Switch>
+            </BrowserRouter>
+          </div>
+        );
+    }
+} 
+const  NoMatch=()=>{
+    console.log("noMatch");
+    return (
+      <div>
+        <h1>No match</h1>
+      </div>
+    );
 }
 
+const  Home=()=>{
+    console.log("Home");
+    return (
+      <div>
+        <h1>You are on HomePage</h1>
+      </div>
+    );
+}
+const  About=()=>{
+    console.log("About");
+    return (
+      <div>
+        <h1>You are on About Page</h1>
+      </div>
+    );
+}
+
+const LocationDisplay = (props) => {
+
+    let location = useLocation();
+    return <div>
+        <h1 data-testid="location-display">{location.pathname}</h1>
+    </div>
+}
 
 export default App;
