c156fa3d4340675b2e0a1ad18bc698c5b99f38f9
Aquib Shaikh <aquibs023@gmail.com>
2021-01-04 13:26:37 +0530

    vheckinh sandbox

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,35 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 import {
-    BrowserRouter ,
-    Switch,
-    Route,
-    Link,
-    useLocation
-  } from "react-router-dom";
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+  useLocation
+} from "react-router-dom";
 
-  
 class App extends Component {
-    constructor(props){
-        super(props);
-    }
-    
-    render() {
+  constructor(props) {
+    super(props);
+  }
 
-        return (
-          <div id="main">
-            <BrowserRouter>
-              <LinkTemplate />
-              <LocationDisplay />
-              
-            </BrowserRouter>
-          </div>
-        );
-    }
-} 
+  render() {
+    return (
+      <div id="main">
+        <BrowserRouter>
+          <LinkTemplate />
+          <LocationDisplay />
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 const LinkTemplate = () => {
   return (
     <div className="menu-div">
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
- 
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+
       <Switch>
         <Route path="/about" component={About} />
         <Route exact path="/" component={Home} />
@@ -40,36 +37,35 @@ const LinkTemplate = () => {
       </Switch>
     </div>
   );
-}
-const  NoMatch=()=>{
-    return (
-      <div>
-        <h1>No match</h1>
-      </div>
-    );
-}
+};
+const NoMatch = () => {
+  return (
+    <div>
+      <h1>No match Just checking</h1>
+    </div>
+  );
+};
 
-const  Home=()=>{
-    console.log("Home");
-    return (
-      <div>
-        <h1>You are home.</h1>
-      </div>
-    );
-}
-const  About=(props)=>{
-    console.log("About");
-    return (
-      <div>
-        <h1>You are on the about page.</h1>
-      </div>
-    );
-}
+const Home = () => {
+  console.log("Home");
+  return (
+    <div>
+      <h1>You are home.</h1>
+    </div>
+  );
+};
+const About = (props) => {
+  console.log("About");
+  return (
+    <div>
+      <h1>You are on the about page.</h1>
+    </div>
+  );
+};
 
-export function LocationDisplay(){
-
-  let {pathname} = useLocation();
-    return <h1 data-testid="location-display">{pathname}</h1>;
+export function LocationDisplay() {
+  let { pathname } = useLocation();
+  return <h1 data-testid="location-display">{pathname}</h1>;
 }
 
 export default App;
